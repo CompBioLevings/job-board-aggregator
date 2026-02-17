2136ba8051d284022063644e244a8728ea8186da
compbiolevings <dlevings@gmail.com>
2026-02-17 15:14:51 -0600

    Updated [shell]: updated auto_run.sh to remove URL specific for my interest

--- auto_run.sh
+++ auto_run.sh
@@ -9,7 +9,5 @@ python3 scripts/merge_data.py
 sleep 2
 printf "Please open web browser at http://localhost:8000 \n"
 sleep 2
-echo "For my customized search open: http://localhost:8000/?title=data+%7C+computational+%7C+bioinformatician+%7C+bioinformatics+%7C+informatics+%7C+stat+%7C+statistics+%7C+programming+%7C+programmer+%7C+scientist+%7C+analyst+%7C+scientific+software+engineer+%7C+research+software+engineer+%7C+scientific+solutions+engineer+%7C+pipeline+developer"
-sleep 2
 python -m http.server 8000
 bash
